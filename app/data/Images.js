ef319acdad9059f2c3228ba15a1e8f13d5ee9d8c
hackma01 <amanmourya295@gmail.com>
2025-08-17 21:29:23 +0530

    Fix: Card color changed, theme corrected

---
+++ app/data/Images.js
@@ -0,0 +1,21 @@
+export const subjectImages = {
+    "Engineering Mathematics":
+      "https://placehold.co/600x400/7C3AED/FFFFFF?text=Calculus",
+    RDBMS: "https://placehold.co/600x400/DB2777/FFFFFF?text=Database",
+    "Computer Networks":
+      "https://placehold.co/600x400/059669/FFFFFF?text=Networks",
+    "Computer Organization":
+      "https://placehold.co/600x400/D97706/FFFFFF?text=Architecture",
+    "Digital Logic":
+      "https://placehold.co/600x400/4F46E5/FFFFFF?text=Logic+Gates",
+    "Operating Systems": "https://placehold.co/600x400/0284C7/FFFFFF?text=OS",
+    Algorithms: "https://placehold.co/600x400/BE185D/FFFFFF?text=Algorithms",
+    "Compiler Design": "https://placehold.co/600x400/6D28D9/FFFFFF?text=Compiler",
+    "Theory of Computation":
+      "https://placehold.co/600x400/047857/FFFFFF?text=TOC",
+    "Data Structures":
+      "https://placehold.co/600x400/4338CA/FFFFFF?text=Data+Structures",
+    "C Programming": "https://placehold.co/600x400/0369A1/FFFFFF?text=C+Code",
+    "Discrete Mathematics":
+      "https://placehold.co/600x400/C026D3/FFFFFF?text=Discrete+Math",
+  };
---
+++ app/data/SubjectLinks.js
@@ -0,0 +1,26 @@
+export const initialSubjectLinks = {
+    "Engineering Mathematics":
+      "https://unacademy.com/course/engineering-mathematics-for-data-science/HJTCB228",
+    RDBMS:
+      "https://unacademy.com/course/comprehensive-course-on-dbms-for-cs-&-da/VQZLADB0",
+    "Computer Networks":
+      "https://unacademy.com/course/comprehensive-course-in-computer-networks-386/M7HJC525",
+    "Computer Organization":
+      "https://unacademy.com/course/course-on-computer-organization-&-architecture-gate---23-&-24/20MLC86V",
+    "Digital Logic":
+      "https://unacademy.com/course/comprehensive-course-on-digital-logic-202/1Y7MHDY8",
+    "Operating Systems":
+      "https://unacademy.com/course/comprehensive-course-on-operating-system/UY44YXAO",
+    Algorithms:
+      "https://unacademy.com/course/complete-course-on-algorithms-for-csit/ZPSB16Y9",
+    "Compiler Design":
+      "https://unacademy.com/course/complete-course-on-compiler-design/21ZNGIKB",
+    "Theory of Computation":
+      "https://unacademy.com/course/complete-course-on-theory-of-computation-for-gate-cs-&-it/HQ10718Q",
+    "Data Structures":
+      "https://unacademy.com/course/course-on-data-structure/4IJKQO8D",
+    "C Programming":
+      "https://unacademy.com/course/comprehensive-course-on-c--programming/WY4D3M9Q",
+    "Discrete Mathematics":
+      "https://unacademy.com/course/complete-course-on-discrete-mathematics/CO76I25O",
+  };
